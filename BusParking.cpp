518c7fc74af10e8b65931ae9d9a9b58fcda9089a
DuongNguyen <nhatduong30001@gmail.com>
2021-01-03 18:14:47 +0700

    I finidh the add function in RB tree

--- BusParking.cpp
+++ BusParking.cpp
@@ -1,13 +1,15 @@
+// Interval tree
+// Segment tree
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
+// Test
 class Interval
 {
 public:
     int start;
     int end;
-    vector<Interval> intervals;
     Interval(int start = 0, int end = 0)
     {
         this->start = start;
@@ -16,7 +18,93 @@ public:
 };
 class BusParking
 {
+public:
+    class Node
+    {
+        int height;
+        int max;
+        Node *pLeft;
+        Node *pRight;
+        Interval interval;
+        friend class BusParking;
+
+    public:
+        Node(Interval _interval) : interval(_interval), pLeft(NULL), pRight(NULL), height(1), max(_interval.end) {}
+        ~Node() {}
+    };
+    void insert(int s, int t)
+    {
+        Interval new_interval(s, t);
+        insert(this->root, new_interval);
+    }
+    BusParking()
+    {
+        minPark = 0;
+        root = nullptr;
+    }
+    ~BusParking()
+    {
+        clear(this->root);
+    }
+
+private:
+    int minPark;
+    Node *root;
+    void insert(Node *_root, Interval &value);
+    void clear(Node *_root)
+    {
+        if (_root)
+        {
+            clear(_root->pLeft);
+            clear(_root->pRight);
+            delete _root;
+        }
+    }
+    int BalanceFactor(Node *);
+    int Height(Node *);
+    int getMinBus(Interval temp);
 };
+int BusParking::Height(Node *_root)
+{
+    if (_root == nullptr)
+        return 0;
+    else
+    {
+        return _root->height;
+    }
+}
+int BusParking::BalanceFactor(Node *_root)
+{
+    if (_root)
+    {
+        return Height(_root->pLeft) - Height(_root->pRight);
+    }
+    return 0;
+}
+void BusParking::insert(Node *_root, Interval &value)
+{
+    if (_root == nullptr)
+    {
+        Node *temp = new Node(value);
+        _root = temp;
+        return;
+    }
+    else
+    {
+        if (_root->max < value.end)
+        {
+            _root->max = value.end;
+        }
+        if (value.start < _root->interval.start)
+        {
+            insert(_root->pLeft, value);
+        }
+        else
+        {
+            insert(_root->pRight, value);
+        }
+    }
+}
 int minPark(vector<Interval> lInterval)
 {
     int i = 0, j = 1;
--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <queue>
+#include <math.h>
 using namespace std;
+void printNSpace(int n)
+{
+    for (int i = 0; i < n - 1; i++)
+        cout << " ";
+}
+void printInteger(int &n)
+{
+    cout << n << " ";
+}
 enum Color
 {
     Red,
@@ -14,25 +25,119 @@ struct Node
     Node *pParent;
     Node()
     {
-        pLeft = pRight = pParent = nullptr;
+        pLeft = pRight = pParent = NULL;
         color = Black;
     }
     Node(int _key)
     {
         key = _key;
         color = Black;
-        pLeft = pRight = pParent = nullptr;
+        pLeft = pRight = pParent = NULL;
     }
 };
 class RedBlackTree
 {
     Node *root;
-    void Left_Rotation(Node *&);
-    void Right_Rotation(Node *&);
+    void NLR_traverse(Node *_root)
+    {
+        if (_root)
+        {
+            cout << _root->key << "_" << _root->color << " ";
+            NLR_traverse(_root->pLeft);
+            NLR_traverse(_root->pRight);
+        }
+    }
+    void LRN_traverse(Node *_root)
+    {
+        if (_root)
+        {
+            LRN_traverse(_root->pLeft);
+            LRN_traverse(_root->pRight);
+            cout << _root->key << "_" << _root->color << " ";
+        }
+    }
+    void Left_Rotation(Node *);
+    void Right_Rotation(Node *);
     void insert(Node *&);
-    void InsertFixup(Node *&);
+    void InsertFixup(Node *);
+    void clear(Node *&root)
+    {
+        if (root)
+        {
+            clear(root->pLeft);
+            clear(root->pParent);
+            delete root;
+        }
+    }
+    int getHeightRec(Node *node)
+    {
+        if (node == NULL)
+            return 0;
+        int lh = this->getHeightRec(node->pLeft);
+        int rh = this->getHeightRec(node->pRight);
+        return (lh > rh ? lh : rh) + 1;
+    }
 
 public:
+    void NLR_traverse()
+    {
+        NLR_traverse(this->root);
+    }
+    void LRN_traverse()
+    {
+        LRN_traverse(this->root);
+    }
+    int getHeight()
+    {
+        return this->getHeightRec(this->root);
+    }
+    void printTreeStructure()
+    {
+        int height = this->getHeight();
+        if (this->root == NULL)
+        {
+            cout << "NULL\n";
+            return;
+        }
+        queue<Node *> q;
+        q.push(root);
+        Node *temp;
+        int count = 0;
+        int maxNode = 1;
+        int level = 0;
+        int space = pow(2, height);
+        printNSpace(space / 2);
+        while (!q.empty())
+        {
+            temp = q.front();
+            q.pop();
+            if (temp == NULL)
+            {
+                cout << " ";
+                q.push(NULL);
+                q.push(NULL);
+            }
+            else
+            {
+                cout << temp->key << "_" << temp->color;
+                q.push(temp->pLeft);
+                q.push(temp->pRight);
+            }
+            printNSpace(space);
+            count++;
+            if (count == maxNode)
+            {
+                cout << endl;
+                count = 0;
+                maxNode *= 2;
+                level++;
+                space /= 2;
+                printNSpace(space / 2);
+            }
+            if (level == height)
+                return;
+        }
+    }
     RedBlackTree()
     {
         root = NULL;
@@ -45,8 +150,13 @@ public:
         Node *new_node = new Node(_key);
         insert(new_node);
     }
+    void clear()
+    {
+        clear(this->root);
+        this->root = NULL;
+    }
 };
-void RedBlackTree::Left_Rotation(Node *&_root)
+void RedBlackTree::Left_Rotation(Node *_root)
 {
     Node *y = _root->pRight;
     _root->pRight = y->pLeft;
@@ -69,7 +179,7 @@ void RedBlackTree::Left_Rotation(Node *&_root)
     _root->pParent = y;
     //_root = y;
 }
-void RedBlackTree::Right_Rotation(Node *&_root)
+void RedBlackTree::Right_Rotation(Node *_root)
 {
     Node *y = _root->pLeft;
     _root->pLeft = y->pRight;
@@ -96,7 +206,7 @@ void RedBlackTree::insert(Node *&new_node)
 {
     Node *prev = NULL;
     Node *temp = this->root;
-    while (temp != nullptr)
+    while (temp != NULL)
     {
         prev = temp;
         if (new_node->key < temp->key)
@@ -109,7 +219,9 @@ void RedBlackTree::insert(Node *&new_node)
     new_node->pParent = prev;
     if (prev == NULL)
     {
+        new_node->color = Black;
         this->root = new_node;
+        return;
     }
     else if (new_node->key < prev->key)
         prev->pLeft = new_node;
@@ -121,32 +233,119 @@ void RedBlackTree::insert(Node *&new_node)
     new_node->color = Red;
     InsertFixup(new_node);
 }
-void RedBlackTree::InsertFixup(Node *&fixed_Node)
+void RedBlackTree::InsertFixup(Node *fixed_Node)
 {
-    while (fixed_Node->pParent->color == Red)
+    while (fixed_Node->pParent && fixed_Node->pParent->color == Red)
     {
         if (fixed_Node->pParent == fixed_Node->pParent->pParent->pLeft)
         {
             Node *temp = fixed_Node->pParent->pParent->pRight;
-            if (temp->color == Red)
-            //Case1
+            if (temp)
             {
-                fixed_Node->pParent->color = Black;
-                temp->color = Black;
-                fixed_Node->pParent->pParent->color = Red;
-                fixed_Node = fixed_Node->pParent->pParent;
+                if (temp->color == Red)
+                //Case1
+                {
+                    fixed_Node->pParent->color = Black;
+                    temp->color = Black;
+                    fixed_Node->pParent->pParent->color = Red;
+                    fixed_Node = fixed_Node->pParent->pParent;
+                }
+                else
+                {
+                    if (fixed_Node == fixed_Node->pParent->pRight)
+                    // Case 2 we rotate to become case 3
+                    {
+                        fixed_Node = fixed_Node->pParent;
+                        Left_Rotation(fixed_Node);
+                    }
+                    // This is case 3
+                    fixed_Node->pParent->color = Black;
+                    fixed_Node->pParent->pParent->color = Red;
+                    Right_Rotation(fixed_Node->pParent->pParent);
+                }
+                //The cases are symmetric
             }
             else
             {
                 if (fixed_Node == fixed_Node->pParent->pRight)
+                // Case 2 we rotate to become case 3
                 {
                     fixed_Node = fixed_Node->pParent;
                     Left_Rotation(fixed_Node);
                 }
+                // This is case 3
+                fixed_Node->pParent->color = Black;
+                fixed_Node->pParent->pParent->color = Red;
+                Right_Rotation(fixed_Node->pParent->pParent);
             }
         }
         else
         {
+            Node *temp = fixed_Node->pParent->pParent->pLeft;
+            if (temp)
+            {
+                if (temp->color == Red)
+                {
+                    fixed_Node->pParent->color = Black;
+                    temp->color = Black;
+                    fixed_Node->pParent->pParent->color = Red;
+                    fixed_Node = fixed_Node->pParent->pParent;
+                }
+                else
+                {
+                    if (fixed_Node == fixed_Node->pParent->pLeft)
+                    {
+                        fixed_Node = fixed_Node->pParent;
+                        Right_Rotation(fixed_Node);
+                    }
+                    fixed_Node->pParent->color = Black;
+                    fixed_Node->pParent->pParent->color = Red;
+                    Left_Rotation(fixed_Node->pParent->pParent);
+                }
+            }
+            else
+            {
+                if (fixed_Node == fixed_Node->pParent->pLeft)
+                {
+                    fixed_Node = fixed_Node->pParent;
+                    Right_Rotation(fixed_Node);
+                }
+                fixed_Node->pParent->color = Black;
+                fixed_Node->pParent->pParent->color = Red;
+                Left_Rotation(fixed_Node->pParent->pParent);
+            }
         }
     }
+    this->root->color = Black;
+}
+int main()
+{
+    RedBlackTree test_tree;
+    test_tree.insert(5);
+    test_tree.insert(11);
+    int arr[] = {10, 52, 98, 32, 68, 92, 48, 13, 42, 63, 99, 100, 50, 40, 12, 11, 9, 0, 1, 3, 7};
+    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+    {
+        test_tree.insert(arr[i]);
+    }
+    test_tree.NLR_traverse();
+    cout << endl;
+    test_tree.LRN_traverse();
+    // test_tree.insert(10);
+    // test_tree.insert(52);
+    // test_tree.insert(98);
+    // test_tree.insert(32);
+    // test_tree.insert(68);
+    // test_tree.insert(92);
+    // test_tree.insert(48);
+    // test_tree.insert(13);
+    // test_tree.insert(42);
+    // test_tree.insert(63);
+    // test_tree.insert(99);
+    // test_tree.insert(100);
+    // test_tree.insert(50);
+    // test_tree.insert(40);
+    // test_tree.printTreeStructure();
+    system("pause");
+    return 0;
 }
